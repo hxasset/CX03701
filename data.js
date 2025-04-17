17336bde7725b4f63c47aa904d807ecf921502ff
hxasset <hxasset0307@gmail.com>
2025-04-17 17:40:19 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,209 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-l",
+      "name": "L",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.6928476817766462,
+        "pitch": 0.12132043539991955,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.126544924032528,
+          "pitch": 0.627815358838232,
+          "rotation": 5.497787143782138,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.706537811946113,
+          "pitch": -0.31449667360218925,
+          "title": "匯幸資產版權所有",
+          "text": "<span data-contrast=\"auto\" xml:lang=\"ZH-TW\" lang=\"ZH-TW\" class=\"TextRun SCXW18864576 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); white-space-collapse: preserve; background-color: rgb(255, 255, 255); font-size: 11pt; line-height: 23px; font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-variant-ligatures: none !important;\"><span class=\"NormalTextRun SCXW18864576 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text;\">202504</span></span><span class=\"LineBreakBlob BlobObject DragDrop SCXW18864576 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); white-space-collapse: preserve; background-color: rgb(255, 255, 255); font-size: 11pt; line-height: 23px; font-family: WordVisiCarriageReturn_MSFontService, &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif;\"><span class=\"SCXW18864576 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; text-wrap-mode: nowrap !important;\">&nbsp;</span><br class=\"SCXW18864576 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; text-wrap-mode: nowrap !important;\"></span>"
+        },
+        {
+          "yaw": 2.6951404321092447,
+          "pitch": -0.20667394826877938,
+          "title": "格局圖",
+          "text": "<img src='floorplanL.jpg'>"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.3386325004571598,
+        "pitch": 0.010379913235704308,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0775988487347163,
+          "pitch": 0.38363062763479583,
+          "rotation": 0.7853981633974483,
+          "target": "3-3"
+        },
+        {
+          "yaw": 2.4867223624244446,
+          "pitch": 0.28389909101873023,
+          "rotation": 5.497787143782138,
+          "target": "2-2"
+        },
+        {
+          "yaw": -1.979401139821583,
+          "pitch": 0.1966204501774662,
+          "rotation": 0.7853981633974483,
+          "target": "0-l"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3563880395519607,
+          "pitch": -0.21758756075406538,
+          "title": "格局圖",
+          "text": "<img src='floorplan1.jpg'>"
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.42920852013329913,
+        "pitch": 0.16476512845376412,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.776381584163154,
+          "pitch": 0.4342770798185249,
+          "rotation": 0.7853981633974483,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3822452521318631,
+          "pitch": -0.4856239602170387,
+          "title": "格局圖",
+          "text": "<img src='floorplan2.jpg'>"
+        }
+      ]
+    },
+    {
+      "id": "3-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.112661427367952,
+        "pitch": 0.21024253404077875,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.980041147519291,
+          "pitch": 0.7810509957140503,
+          "rotation": 0.7853981633974483,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4223189145733928,
+          "pitch": -0.3347711702586089,
+          "title": "格局圖",
+          "text": "<img src='floorplan3.jpg'>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
